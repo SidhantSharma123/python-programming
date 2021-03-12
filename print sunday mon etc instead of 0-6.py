a12c3b295259db587aca1c485b820dec9f86d53e
SIDHANT SHARMA <58850109+SidhantSharma123@users.noreply.github.com>
2021-03-12 23:08:59 +0530

    Add files via upload

---
+++ print sunday mon etc instead of 0-6.py
@@ -0,0 +1,6 @@
+from datetime import date
+today=today.day
+wd=date.weekday(today)
+days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+print("weekday no:",wd)
+print("weekday",days[wd])
